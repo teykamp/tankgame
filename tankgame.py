f4cf36544b06366a30d6570097751e22dd87d41f
thmeyk <thomas@eykamp.com>
2012-12-23 22:16:32 +0100

    Replace typed code w/ code from tutorial website
    Now it includes all the comments I didn't want to type!

--- tankgame.py
+++ tankgame.py
@@ -1,35 +1,45 @@
+#!/usr/bin/env python
+
 try:
     import sys, os, math, random
     import pygame
     from pygame.locals import *
 
 except ImportError, err:
-    print "%s Failed to Load Module: %s" % (__file__,  err)
+    print "%s Failed to Load Module: %s" % (__file__, err)
     sys.exit(1)
 
-
-
 class Paddle(pygame.sprite.Sprite):
-    """docstring for Paddle"""
+    """A paddle sprite. Subclasses the pygame sprite class.
+    Handles its own position so it will not go off the screen."""
+
     def __init__(self, xy):
+        # initialize the pygame sprite part
         pygame.sprite.Sprite.__init__(self)
+        # set image and rect
         self.image = pygame.image.load(os.path.join('images','pong_paddle.gif'))
         self.rect = self.image.get_rect()
 
+        # set position
         self.rect.centerx, self.rect.centery = xy
 
+        # the movement speed of our paddle
         self.movementspeed = 5
 
+        # the current velocity of the paddle -- can only move in Y direction
         self.velocity = 0
 
     def up(self):
-        self.velocity-= self.movementspeed
+        """Increases the vertical velocity"""
+        self.velocity -= self.movementspeed
 
     def down(self):
-        self.velocity+= self.movementspeed
+        """Decreases the vertical velocity"""
+        self.velocity += self.movementspeed
 
     def move(self, dy):
-        if self.rect.bottom + dy >400:
+        """Move the paddle in the y direction. Don't go out the top or bottom"""
+        if self.rect.bottom + dy > 400:
             self.rect.bottom = 400
         elif self.rect.top + dy < 0:
             self.rect.top = 0
@@ -37,110 +47,129 @@ class Paddle(pygame.sprite.Sprite):
             self.rect.y += dy
 
     def update(self):
-            self.move(self.velocity)    
-
-
-
+        """Called to update the sprite. Do this every frame. Handles
+        moving the sprite by its velocity"""
+        self.move(self.velocity)
 
 class Game(object):
-    """docstring for Game"""
-    
+    """Our game object! This is a fairly simple object that handles the
+    initialization of pygame and sets up our game to run."""
+
     def __init__(self):
-        
-        pygame.init () 
+        """Called when the the Game object is initialized. Initializes
+        pygame and sets up our pygame window and other pygame tools
+        that we will need for more complicated tutorials."""
 
-        self.window = pygame.display.set_mode ((800, 400))
+        # load and set up pygame
+        pygame.init()
 
-        self.clock = pygame.time.Clock ()
+        # create our window
+        self.window = pygame.display.set_mode((800, 400))
 
-        pygame.display.set_caption("PONG")
+        # clock for ticking
+        self.clock = pygame.time.Clock()
 
-        pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP]) 
-        
-        self.backround = pygame.Surface((800, 400))
-        self.backround.fill ((255,255,255))
-        pygame.draw.line(self.backround, (0,0,0), (400,0), (400,400), 2)
-        self.window.blit(self.backround, (0,0))
+        # set the window title
+        pygame.display.set_caption('Pong')
+
+        # tell pygame to only pay attention to certain events
+        # we want to know if the user hits the X on the window, and we
+        # want keys so we can close the window with the esc key
+        pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP])
+
+        # make background -- all white, with black line down the middle
+        self.background = pygame.Surface((800,400))
+        self.background.fill((255,255,255))
+        # draw the line vertically down the center
+        pygame.draw.line(self.background, (0,0,0), (400,0), (400,400), 2)
+        self.window.blit(self.background, (0,0))
+        # flip the display so the background is on there
         pygame.display.flip()
 
+        # a sprite rendering group for our ball and paddles
         self.sprites = pygame.sprite.RenderUpdates()
 
-        self.leftpaddle = Paddle ((50,200))
-        self.sprites.add (self.leftpaddle)
-        self.rightpaddle = Paddle ((750,200))
-        self.sprites.add (self.rightpaddle)
+        # create our paddles and add to sprite group
+        self.leftpaddle = Paddle((50,200))
+        self.sprites.add(self.leftpaddle)
+        self.rightpaddle = Paddle((750,200))
+        self.sprites.add(self.rightpaddle)
 
     def run(self):
-        
+        """Runs the game. Contains the game loop that computes and renders
+        each frame."""
+
         print 'Starting Event Loop'
 
         running = True
+        # run until something tells us to stop
         while running:
+
+            # tick pygame clock
+            # you can limit the fps by passing the desired frames per seccond to tick()
             self.clock.tick(60)
 
+            # handle pygame events -- if user closes game, stop running
             running = self.handleEvents()
 
-            pygame.display.set_caption('PONG  %d fps' % self.clock.get_fps())
+            # update the title bar with our frames per second
+            pygame.display.set_caption('Pong   %d fps' % self.clock.get_fps())
 
-
+            # update our sprites
             for sprite in self.sprites:
                 sprite.update()
 
-            self.sprites.clear(self.window, self.backround)
-            dirty = self.sprites.draw(self.window)
+            # render our sprites
+            self.sprites.clear(self.window, self.background)    # clears the window where the sprites currently are, using the background
+            dirty = self.sprites.draw(self.window)              # calculates the 'dirty' rectangles that need to be redrawn
 
-            pygame.display.update(dirty)
+            # blit the dirty areas of the screen
+            pygame.display.update(dirty)                        # updates just the 'dirty' areas
 
-        print 'Thanks for Playing!!!!!'
-
-
+        print 'Quitting. Thanks for playing'
 
     def handleEvents(self):
-        
+        """Poll for PyGame events and behave accordingly. Return false to stop
+        the event loop and end the game."""
+
+        # poll for pygame events
         for event in pygame.event.get():
             if event.type == QUIT:
                 return False
 
-
+            # handle user input
             elif event.type == KEYDOWN:
-
+                # if the user presses escape, quit the event loop.
                 if event.key == K_ESCAPE:
                     return False
 
+                # paddle control
                 if event.key == K_w:
                     self.leftpaddle.up()
-
                 if event.key == K_s:
                     self.leftpaddle.down()
 
-
                 if event.key == K_UP:
                     self.rightpaddle.up()
-
                 if event.key == K_DOWN:
                     self.rightpaddle.down()
 
             elif event.type == KEYUP:
-
-               
+                # paddle control
                 if event.key == K_w:
                     self.leftpaddle.down()
-
                 if event.key == K_s:
                     self.leftpaddle.up()
 
-
                 if event.key == K_UP:
                     self.rightpaddle.down()
-
                 if event.key == K_DOWN:
                     self.rightpaddle.up()
 
         return True
 
 
+# create a game and run it
 if __name__ == '__main__':
     game = Game()
     game.run()
-
-
